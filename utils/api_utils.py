cd25c0ff5d53a33d8c7b19178e666dad30140cf3
happyd <deaytthwt@gmail.com>
2026-07-18 01:23:35 +0600

    add logging decorator

--- utils/api_utils.py
+++ utils/api_utils.py
@@ -1,4 +1,21 @@
+import json
+import curlify
+import requests
+from logging import Logger
 from requests import Session
+
+from utils.jsons_utils import JsonUtils
+
+
+def log_response(func):
+    def _wrapper(*args, **kwargs) -> requests.Response:
+        response = func(*args, **kwargs)
+        Logger.info(f"Request: {curlify.to_curl(response.request)}")
+        body = json.dumps(response.json(), indent=2) if JsonUtils.is_json(response.text) else response.text
+        Logger.info(f"Response status code ='{response.status_code}', elapsed_time='{response.elapsed}'\n{body}\n")
+        return response
+
+    return _wrapper
 
 
 class ApiUtils:
@@ -10,10 +27,12 @@ class ApiUtils:
         self.session.headers.update(headers)
         self.url = url
 
+    @log_response
     def get(self, endpoint_url, **kwargs):
         response = self.session.get(self.url + endpoint_url, **kwargs)
         return response
 
+    @log_response
     def post(self, endpoint_url, data=None, json=None, **kwargs):
         response = self.session.post(self.url + endpoint_url, data=data, json=json, **kwargs)
         return response
